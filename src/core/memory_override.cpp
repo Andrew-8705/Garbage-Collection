900faaf8611acac48436e79e8a1408716e2b6289
Andrew-8705 <abaldin05@mail.ru>
2025-11-30 14:16:40 +0300

    fix: Some changes in memory override

--- src/core/memory_override.cpp
+++ src/core/memory_override.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
 #include <cstdlib>
 #include <new>
+#include <string>
 
 #include "config.hpp" 
+#include "logger.hpp"
 
 #ifdef GC_DEBUG_ALLOC
 
 void* operator new(std::size_t size) noexcept(false) {
-    std::cerr << "[Global New] Allocated: " << size << " bytes.\n";
-    
+    //std::cerr << "[Global New] Allocated: " << size << " bytes.\n";
+
     if (void* p = std::malloc(size)) {
+        Logger::getInstance().log((long long)size);
         return p;
     }
     throw std::bad_alloc{};
@@ -17,7 +20,7 @@ void* operator new(std::size_t size) noexcept(false) {
 
 void operator delete(void* p) noexcept {
     if (p == nullptr) return;
-    std::cerr << "[Global Delete] Deallocating memory.\n";
+    //std::cerr << "[Global Delete] Deallocating memory.\n";
     std::free(p);
 }
 
