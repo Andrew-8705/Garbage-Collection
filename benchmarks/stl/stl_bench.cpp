7f1ea0ab836a88897b9361a7d8da63e1d4993dc9
Andrew-8705 <abaldin05@mail.ru>
2026-04-15 20:56:27 +0300

    refactor: Update container types to use GC-aware versions and add Tracy profiling macros

--- benchmarks/stl/stl_bench.cpp
+++ benchmarks/stl/stl_bench.cpp
@@ -17,26 +17,26 @@
 #include "../../include/impl/BoehmGCAdapter.hpp"
 #endif
 
+#ifdef TRACY_ENABLE
+    #include <tracy/Tracy.hpp>
+#else
+    #define ZoneScoped
+    #define ZoneScopedN(x)
+    #define FrameMark
+#endif
 
 const int POOL_SIZE = 500;       // сколько контейнеров будет создано
 const int CHUNK_SIZE = 200;      // сколько элементов будет добавлено в контейнер
-const int TOTAL_OPERATIONS = 50; // сколько операций для выбранного контейнера будет выполнено
+const int TOTAL_OPERATIONS = 500'000; // сколько операций для выбранного контейнера будет выполнено
 
 namespace gc {
 
 using GCString = std::basic_string<char, std::char_traits<char>, gc::Allocator<char>>;
 
-template <typename T>
-using GCVector = std::vector<T, gc::Allocator<T>>;
-
-template <typename T>
-using GCList = std::list<T, gc::Allocator<T>>;
-
-template <typename K, typename V>
-using GCMap = std::map<K, V, std::less<K>, gc::Allocator<std::pair<const K, V>>>;
-
-template <typename K, typename V>
-using GCUnorderedMap = std::unordered_map<K, V, std::hash<K>, std::equal_to<K>, gc::Allocator<std::pair<const K, V>>>;
+template <typename T> using GCVector = std::vector<T, gc::Allocator<T>>;
+template <typename T> using GCList = std::list<T, gc::Allocator<T>>;
+template <typename K, typename V> using GCMap = std::map<K, V, std::less<K>, gc::Allocator<std::pair<const K, V>>>;
+template <typename K, typename V> using GCUnorderedMap = std::unordered_map<K, V, std::hash<K>, std::equal_to<K>, gc::Allocator<std::pair<const K, V>>>;
 
 // ---------------------------------------------------------
 
@@ -45,16 +45,19 @@ private:
     GCVector<GCString> data;
 public:
     void grow() override {
+        ZoneScopedN("Vector_Grow");
         for (int i = 0; i < CHUNK_SIZE; i++) {
             data.push_back(GCString("x") + GCString(std::to_string(i).c_str()));
         }
     }
 
     void shrink() override {
+        ZoneScopedN("Vector_Shrink");
         if (data.size() > CHUNK_SIZE) data.resize(data.size() - CHUNK_SIZE);
     }
 
     void access() override {
+        ZoneScopedN("Vector_Access");
         if (!data.empty()) {
             const GCString& val = data[rand() % data.size()];
             (void)val;
@@ -69,16 +72,19 @@ private:
     GCList<int> data;
 public:
     void grow() override {
+        ZoneScopedN("List_Grow");
         for (int i = 0; i < CHUNK_SIZE; i++) 
             data.push_back(i);
     }
 
     void shrink() override {
+        ZoneScopedN("List_Shrink");
         for (int i = 0; i < CHUNK_SIZE / 2 && !data.empty(); i++) 
             data.pop_back();
     }
 
     void access() override {
+        ZoneScopedN("List_Access");
         if (!data.empty()) {
             volatile int val = *std::next(data.begin(), rand() % data.size());
             (void)val;
@@ -90,22 +96,25 @@ public:
 
 class MapContainer : public AbstractContainer {
 private:
-    std::map<int, GCString> data;
+    GCMap<int, GCString> data;
     int next_key = 0;
 public: 
     void grow() override {
+        ZoneScopedN("Map_Grow");
         for (int i = 0; i < CHUNK_SIZE; i++) {
             data[next_key++] = GCString("map_val") + GCString(std::to_string(i).c_str());
         }
     }
 
     void shrink() override {
+        ZoneScopedN("Map_Shrink");
         for (int i = 0; i < CHUNK_SIZE / 2 && !data.empty(); ++i) {
             data.erase(data.begin());
         }
     }
 
     void access() override {
+        ZoneScopedN("Map_Access");
         if (!data.empty()) {
             if (next_key > 0) {
                  auto it = data.find(rand() % next_key);
@@ -119,22 +128,25 @@ public:
 
 class UnorderedMapContainer : public AbstractContainer {
 private:
-    std::unordered_map<int, int> data;
+    GCUnorderedMap<int, int> data;
     int next_key = 0;
 public:
     void grow() override {
+        ZoneScopedN("UnorderedMap_Grow");
         for (int i = 0; i < CHUNK_SIZE; ++i) {
             data[next_key++] = i;
         }
     }
 
     void shrink() override {
+        ZoneScopedN("UnorderedMap_Shrink");
         for (int i = 0; i < CHUNK_SIZE / 2 && !data.empty(); ++i) {
             data.erase(data.begin());
         }
     }
 
     void access() override {
+        ZoneScopedN("UnorderedMap_Access");
         if (!data.empty()) {
              if (next_key > 0) {
                 auto it = data.find(rand() % next_key);
@@ -153,12 +165,14 @@ private:
     const size_t APPEND_SIZE = 100;
 public:
     void grow() override {
+        ZoneScopedN("String_Grow");
         for (int i = 0; i < 5; ++i) { 
             data += GCString(APPEND_SIZE, ' ');
         }
     }
 
     void shrink() override {
+        ZoneScopedN("String_Shrink");
         if (data.size() > APPEND_SIZE) {
             data.resize(data.size() - APPEND_SIZE);
         } else {
@@ -167,6 +181,7 @@ public:
     }
 
     void access() override {
+        ZoneScopedN("String_Access");
         if (!data.empty()) {
             char c = data[0];
             (void)c;
@@ -180,6 +195,8 @@ public:
 
 
 int main() {
+    ZoneScopedN("Main_Function");
+
     #ifdef HAS_BDWGC
         static gc::BoehmGCAdapter gc_instance;
         std::cout << "Strategy: Boehm-Demers-Weiser GC\n";
@@ -189,9 +206,8 @@ int main() {
     #endif
 
     gc::MemoryManager::setInstance(&gc_instance);
-
-
     Logger::getInstance();
+
     // std::srand(std::time(0));
     // std::random_device rd;
     // std::mt19937 gen(rd());
@@ -201,34 +217,35 @@ int main() {
     std::uniform_int_distribution<> op_dist(0, 2); // 0:grow, 1:shrink, 2:access
     std::uniform_int_distribution<> type_dist(0, 4); // 0:Vector, 1:List, 2:Map, 3:UnorderedMap, 4:String
 
-    std::vector<std::unique_ptr<gc::AbstractContainer>> pool;
+    gc::GCVector<gc::AbstractContainer*> pool;
     
     // 1. Заполнение пула контейнерами
     for (int i = 0; i < POOL_SIZE; i++) {
         int type = type_dist(gen);
         switch (type) {
             case 0: { 
-                pool.push_back(std::make_unique<gc::VectorContainer>());
+                // Используем gc::make вместо make_unique
+                pool.push_back(gc::make<gc::VectorContainer>());
                 Logger::getInstance().log("Created: VectorContainer"); 
                 break;
             }
             case 1: {
-                pool.push_back(std::make_unique<gc::ListContainer>());
+                pool.push_back(gc::make<gc::ListContainer>());
                 Logger::getInstance().log("Created: ListContainer");
                 break;
             }
             case 2: {
-                pool.push_back(std::make_unique<gc::MapContainer>());
+                pool.push_back(gc::make<gc::MapContainer>());
                 Logger::getInstance().log("Created: MapContainer");
                 break;
             }
             case 3: {
-                pool.push_back(std::make_unique<gc::UnorderedMapContainer>());
+                pool.push_back(gc::make<gc::UnorderedMapContainer>());
                 Logger::getInstance().log("Created: UnorderedMapContainer");
                 break;
             }
             case 4: {
-                pool.push_back(std::make_unique<gc::StringContainer>());
+                pool.push_back(gc::make<gc::StringContainer>());
                 Logger::getInstance().log("Created: StringContainer");
                 break;
             }
@@ -242,8 +259,9 @@ int main() {
 
     // 2. Главный цикл рандомизированных операций
     for (int i = 0; i < TOTAL_OPERATIONS; i++) {
+        ZoneScopedN("Benchmark_Iteration");
         int target_idx = pool_dist(gen);
-        gc::AbstractContainer* target = pool[target_idx].get();
+        gc::AbstractContainer* target = pool[target_idx]; 
         int op_type = op_dist(gen);
         
         const char* op_name = "";
@@ -259,7 +277,8 @@ int main() {
             target->access();
         }
 
-        Logger::getInstance().logFormatted("Op #%d: [%s] %s at index %d",  i, target->getName(), op_name, target_idx);
+        FrameMark;
+        // Logger::getInstance().logFormatted("Op #%d: [%s] %s at index %d",  i, target->getName(), op_name, target_idx);
     }
 
     auto end = std::chrono::high_resolution_clock::now();
@@ -271,8 +290,16 @@ int main() {
     std::cout << "\n[1] Stats BEFORE clearing the pool:\n";
     gc::MemoryManager::getInstance()->printSummary();
 
-    pool.clear();
-    gc::MemoryManager::getInstance()->collect(); 
+    {
+        ZoneScopedN("Clear_Pool_And_Collect");
+        
+        for (auto ptr : pool) {
+            gc::destroy(ptr);
+        }
+        pool.clear();
+        
+        gc::MemoryManager::getInstance()->collect(); 
+    }
 
     std::cout << "\n[2] Stats AFTER clearing the pool:\n";
     gc::MemoryManager::getInstance()->printSummary();
