647ea969fc282c87aa9369aca24876dabc9b061c
Andrew-8705 <abaldin05@mail.ru>
2026-04-15 21:01:03 +0300

    refactor: Add Tracy profiling macros for memory allocation and deallocation

--- src/impl/BoehmGCAdapter.cpp
+++ src/impl/BoehmGCAdapter.cpp
@@ -2,6 +2,15 @@
 #include "../../include/utils/logger.hpp"
 #include <gc.h> // API библиотеки Boehm
 
+#ifdef TRACY_ENABLE
+    #include <tracy/Tracy.hpp>
+#else
+    #define TracyAllocN(ptr, size, name)
+    #define TracyFreeN(ptr, name)
+    #define TracyPlot(name, val)
+    #define ZoneScopedC(x)
+#endif
+
 namespace gc {
 
 BoehmGCAdapter::BoehmGCAdapter() {
@@ -12,26 +21,36 @@ void* BoehmGCAdapter::allocate(size_t size) {
     void* ptr = GC_MALLOC(size);
     
     if (ptr) {
-        Logger::getInstance().logAlloc(size, ptr);
+        TracyAllocN(ptr, size, "BoehmGC");
 
         stats.totalAllocations++;
         stats.allocatedBytes += size; 
+
+        TracyPlot("Total Allocated (Boehm)", (int64_t)stats.allocatedBytes);
+    
+        Logger::getInstance().logAlloc(size, ptr);        
     }
     return ptr;
 }
 
 void BoehmGCAdapter::deallocate(void* ptr) {
     if (ptr) {
+        TracyFreeN(ptr, "BoehmGC");
         GC_FREE(ptr);
         Logger::getInstance().logFree(ptr);
     }
 }
 
 void BoehmGCAdapter::collect() {
-    // Принудительный запуск сборки
+    ZoneScopedC(0xFF0000); 
+
     Logger::getInstance().log("BoehmGC: Force Collect");
+
     GC_gcollect();
     stats.totalCollections++;
+
+    size_t live = GC_get_heap_size() - GC_get_free_bytes();
+    TracyPlot("Live Memory (Boehm)", (int64_t)live);
 }
 
 MemoryStats BoehmGCAdapter::getStats() const {
